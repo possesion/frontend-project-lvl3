390ab7eaf92b8af89bd53f157609d6e73bdfad1f
Gennadiy K <Gennadiy.Kazancev@smekalka.com>
2022-04-21 20:00:54 +0300

    fix babel.config

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,16 @@ const config = {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
-
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
       // Add your rules for custom modules here
       // Learn more about loaders from https://webpack.js.org/loaders/
     ],
